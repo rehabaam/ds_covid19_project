c83f2513a0b5d448d70ab6f04134bbdaf16202c4
Ahmad Samra <rehabaam@gmail.com>
2025-03-16 14:10:51 +0200

    chore(streamlit): updating references to references page

--- reports/streamlit_report/pages/_9. References.py
+++ reports/streamlit_report/pages/_9. References.py
@@ -6,6 +6,26 @@ st.set_page_config(page_title="Covid-19 🦠 Detection", page_icon="🦠")
 st.title("References")
 st.markdown(
     """
-References
+1. [A longitudinal study on the mental health of general population during the COVID-19 epidemic in China - ScienceDirect](https://www.sciencedirect.com/science/article/pii/S0889159120305110)
+
+2. [Covid-19: automatic detection from X-ray images utilizing transfer learning with convolutional neural networks - PubMed](https://pubmed.ncbi.nlm.nih.gov/32524445/)
+
+3. [Automated detection of COVID-19 cases using deep neural networks with X-ray images - PubMed](https://pubmed.ncbi.nlm.nih.gov/32568675/)
+
+4. [[2109.07138] Patch-based Medical Image Segmentation using Matrix Product State Tensor Networks](https://arxiv.org/abs/2109.07138)
+
+5. [Essentials for Radiologists on COVID-19: An Update—Radiology Scientific Expert Panel - PMC](https://pmc.ncbi.nlm.nih.gov/articles/PMC7233379/)
+
+6. [Coronavirus Disease 2019 (COVID-19): Role of Chest CT in Diagnosis and Management | AJR](https://www.ajronline.org/doi/10.2214/AJR.20.22954)
+
+7. [OpenCV](https://opencv.org/)
+
+8. [Signal-to-noise ratio (imaging) - Wikipedia](https://en.wikipedia.org/wiki/Signal-to-noise_ratio_(imaging))
+
+9. [GLCM Texture Features — skimage 0.25.0 documentation](https://scikit-image.org/docs/stable/auto_examples/features_detection/plot_glcm.html)
+
+10. [Ground-glass opacity - Wikipedia](https://en.wikipedia.org/wiki/Ground-glass_opacity)
+
+11. [Tukey's range test - Wikipedia](https://en.wikipedia.org/wiki/Tukey%27s_range_test)
 """
 )
